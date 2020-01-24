a99f2193aa1211e01f4a11f662ae59cfe9c8f232
ubaaiiii <rizqi.ubaidillah15@mhs.ubharajaya.ac.id>
2020-01-25 04:05:27 +0700

    batal online

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ubai.siap-dev.com/';
-$config['site_url'] = 'http://ubai.siap-dev.com/';
+$config['base_url'] = 'http://localhost/penilaian/';
+$config['site_url'] = 'http://localhost/penilaian/';
 
 /*
 |--------------------------------------------------------------------------
